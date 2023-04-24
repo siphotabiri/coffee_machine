3b5850fc68ec897af0677fc02383b3595a243b8d
Siyavuya Mankanku <siphokwesi1@outlook.com>
2023-04-24 17:57:53 +0100

    first commit

---
+++ main.py
@@ -0,0 +1,24 @@
+# Makes 3 Hot Flavours: Expresso, Latte, Cappuccino
+# Expresso: £1.50, 50ml Water, 18g Coffee
+# Latte: £2.50, 200ml Water, 24g Coffe, 150ml Milk
+# Cappuccino: £3.00, 250ml Water, 24g Coffee, 100ml Milk
+#
+# Starting resources in the coffee machine:
+# 300ml Water
+# 200ml Milk
+# 100g of Coffee
+#
+# Coin Operated Machine
+# Penny = 0.01
+# Nickel = 0.05
+# Dime = 0.10
+# Quarter = 0.25
+#
+# Program Requirements:
+#
+# Print Report: machine should be able to tell us what resources it has left
+# Check resources sufficient: look through resources, check it against the recipe and report findings
+# Process coins: calculate how much money coins inserted are worth and change
+# Check transaction successful:
+# Make the coffee: deduct the resources used to make the coffee.
+
