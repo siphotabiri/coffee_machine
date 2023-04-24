5bb4349ac195fd8f5bd21018d6001a69ffcea16b
Siyavuya Mankanku <siphokwesi1@outlook.com>
2023-04-24 18:23:55 +0100

    modified: main.py by adding base dictionary for menu and resource items

--- main.py
+++ main.py
@@ -21,4 +21,35 @@
 # Process coins: calculate how much money coins inserted are worth and change
 # Check transaction successful:
 # Make the coffee: deduct the resources used to make the coffee.
+MENU = {
+    "expresso": {
+        "ingredients": {
+            "water": 50,
+            "milk": 0,
+            "coffee": 18,
+        },
+        "cost": 1.50,
+    },
+    "latte": {
+        "ingredients": {
+            "water": 200,
+            "milk": 150,
+            "coffee": 24,
+        },
+        "cost": 2.50,
+    },
+    "cappuccino": {
+        "ingredients": {
+            "water": 250,
+            "milk": 100,
+            "coffee": 24,
+        },
+        "cost": 3.00,
+    },
+}
 
+resources = {
+    "water": 300,
+    "milk": 200,
+    "coffee": 100,
+}
